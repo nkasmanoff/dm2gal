6fb6e774c17980503d621aa970f2fdfd30760517
nkasmanoff <nkasmanoff@gmail.com>
2020-11-04 08:38:37 -0500

    fix make samples

--- dat/illustris_tng/darkmatter/CDM_field.py
+++ dat/illustris_tng/darkmatter/CDM_field.py
@@ -23,7 +23,7 @@ FILE_TYPE = 'darkmatter/'
 root = '/scratch/nsk367/pytorch-use/research/dm2gal/dat/illustris_tng/' + FILE_TYPE + 'snapshot/'
 # /scratch/gpfs/nk11/dm2gal/dat/illustris-data/hydrosim
 
-prefix_out = 'stellarmass_TNG100-1'
+prefix_out = 'darkmatter_TNG100-1'
 
 f = h5py.File(root+os.listdir(root)[0],'r')
 
@@ -32,33 +32,33 @@ Omega_m  = f['Header'].attrs[u'Omega0']
 BoxSize  = f['Header'].attrs[u'BoxSize']/1e3  #Mpc/h
 Omega_L  = f['Header'].attrs[u'OmegaLambda']
 h        = f['Header'].attrs[u'HubbleParam']
-Nall     = f['Header'].attrs[u'Nsubgroups_Total']
-
+#Nall     = f['Header'].attrs[u'Nsubgroups_Total']
+filenum  = f['Header'].attrs[u'NumFilesPerSnapshot']
+h        = f['Header'].attrs[u'HubbleParam']
+Masses   = f['Header'].attrs[u'MassTable']*1e10  #Msun/h
 #print('Working with %s at redshift %.0f'%(run,redshift))
 
 f_out = '%s_z=%.1f.hdf5'%(prefix_out,round(redshift))
 
-do_Group = False
-do_Subhalo = True
-    # this is always going to be for a hydrodynamic simulation
 f.close()
+
 # do a loop over all subfiles in a given snapshot
 M_total, start = 0.0, time.time()
 for snapshot_tab in os.listdir(root):
     if snapshot_tab != 'wget-log':
         f = h5py.File(root+snapshot_tab,'r')
         #so all header data is the same. 
-                
+              
                 
         ### CDM ###
         pos  = (f['PartType1/Coordinates'][:]/1e3).astype(np.float32)        
-        mass = np.ones(pos.shape[0], dtype=np.float32)*Masses[1] #Msun/h
+        mass = np.ones(pos.shape[0], dtype=np.float32)#   *Masses[1] #Msun/h
         MASL.MA(pos, delta_m, BoxSize, MAS, mass)  #CDM
         M_total += np.sum(mass, dtype=np.float64)            
            
                 
         f.close()
 
-        f = h5py.File(f_out,'w')
-        f.create_dataset('delta_cdm',  data=delta_m)
-        f.close()
+f = h5py.File(f_out,'w')
+f.create_dataset('delta_cdm',  data=delta_m)
+f.close()
---
+++ dat/illustris_tng/darkmatter/CDM_field.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=72:00:00
+#SBATCH --mem=180GB
+#SBATCH --job-name=dmsim
+#SBATCH --mail-type=END
+#SBATCH --output=slurm_%j.out
+
+python CDM_field.py
