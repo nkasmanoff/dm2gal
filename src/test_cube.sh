ef707c118fcb3e2f8f061acf9d250aca32427d81
Noah Kasmanoff <nkasmanoff@gmail.com>
2020-11-08 11:55:38 -0500

    sh script

---
+++ src/test_cube.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=72:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=180GB
+#SBATCH --job-name=test_cube
+#SBATCH --mail-type=END
+#SBATCH --output=slurm_%j.out
+
+python test_cube.py
