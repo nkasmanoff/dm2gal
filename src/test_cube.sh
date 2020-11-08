8e354104eff24b39c80c96a863377f353b579659
nkasmanoff <nkasmanoff@gmail.com>
2020-11-08 12:01:28 -0500

    now testing

--- src/test_cube.sh
+++ src/test_cube.sh
@@ -1,11 +1,11 @@
-
 #!/bin/bash
+
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --time=72:00:00
-#SBATCH --gres=gpu:1
-#SBATCH --mem=180GB
+#SBATCH --gres=gpu:k80:1
+#SBATCH --mem=80GB
 #SBATCH --job-name=test_cube
 #SBATCH --mail-type=END
 #SBATCH --output=slurm_%j.out
